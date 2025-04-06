e668cf489bdeb037c7f096503ae76d81aaebf2c2
nooraizafshan <afshannoreen010@gmail.com>
2025-04-06 12:55:46 +0500

    Added remaining files

---
+++ SQLQuery00.sql
@@ -0,0 +1,12 @@
+﻿USE Careconnect; -- Ensure you're using the correct database
+
+CREATE TABLE Doctors (
+    DoctorID INT IDENTITY(1,1) PRIMARY KEY, -- Auto-incrementing primary key
+    DoctorName NVARCHAR(100) NOT NULL,
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(50) NOT NULL,
+    Specialization NVARCHAR(100) NOT NULL,
+    AvailabilityDays NVARCHAR(100) NOT NULL,
+    Date DATE NOT NULL,
+    Time TIME NOT NULL
+);
---
+++ SQLQuery9.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE Appointments
+ADD Status NVARCHAR(50) DEFAULT 'Pending';
